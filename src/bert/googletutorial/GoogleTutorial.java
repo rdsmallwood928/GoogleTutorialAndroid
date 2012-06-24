7cd031b5665b24d315513049a2332b24f0b9d34c
rdsmallwood928 <rdsmallwood928@gmail.com>
2012-06-24 13:25:41 -0500

    Initial commit for google tutorial

---
+++ src/bert/googletutorial/GoogleTutorial.java
@@ -0,0 +1,15 @@
+package bert.googletutorial;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GoogleTutorial extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
